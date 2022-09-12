b362b9fc95bb8b064414bd693b24aced16178583
Serhii Trachuk <s.trachuk@apiko.com>
2022-09-12 10:10:19 +0300

    Add queue

---
+++ queue/index.js
@@ -0,0 +1,34 @@
+function createQueue() {
+  const queue = []
+
+  return {
+    enqueue(item) {
+      queue.unshift(item)
+    },
+    dequeue() {
+      return queue.pop()
+    },
+    peek() {
+      return queue[queue.length - 1]
+    },
+    get length () {
+      return queue.length
+    },
+    isEmpty () {
+      return queue.length === 0
+    },
+  }
+}
+
+const q = createQueue()
+
+q.enqueue(1)
+q.enqueue(2)
+q.enqueue(3)
+
+q.dequeue() // 1
+q.dequeue() // 2
+q.peek() // 3
+q.isEmpty() // false
+
+
